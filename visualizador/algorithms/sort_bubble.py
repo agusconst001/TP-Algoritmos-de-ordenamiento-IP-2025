8ae0be5629a157113abffbddac429a6c64f224ab
agustinaRodriguez <agustinaconstanzarodriguez@gmail.com>
2025-11-14 20:10:43 -0300

    algoritmos

--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -18,28 +18,23 @@ def init(vals):
 
 def step():
     global items, n, i, j
-    #-------- evalua si i ha llegado al final de la lista
     if i >= n - 1:
         return {"done": True}
     
-    #--------- se definen dos indices a y b para comparar
     a = j 
     b = j + 1 
     swap = False  #indica si se hizo un intercambio
     
-    max_j = n - i - 1 ############
+    max_j = n - i - 1
     
-    #-------- compara e intercambia si es necesario
     if a < max_j:
         if items[a] > items[b]:
             items[a], items[b] = items[b], items[a]
             swap = True
             
-        # Avanzar el puntero j para la siguiente comparación
         j += 1
         
         return {"a": a, "b": b, "swap": swap, "done": False}
-    ###################################
     else:
         i += 1
         j = 0
